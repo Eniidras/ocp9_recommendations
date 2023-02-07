c91035c2105f552bfcab0ef79e8e34b8a22950de
Eniidras <eniidras@yahoo.com>
2023-02-07 23:48:50 +0100

    maj de main et model pour le déploiement

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from flask import Flask, jsonify, render_template, request
 import model
+import os
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = "T0to_na_p4s_d0rm1"
 
 @app.route("/", methods = ['GET'])
 def home():
-    imgs = os.listdir("static/images/")
-    for img in imgs:
-        flash(img, "img")
     return render_template('home.html')
 
 def no_body(message):
@@ -25,12 +23,24 @@ def api():
     json_data = request.json
     if "user_id" not in json_data.keys():
         return no_body("Le corps de la requête n'a pas de champs 'user_id'.")
+
     user_id = json_data["user_id"]
 
-    recommendations = model.recommend(user_id)
+    if "filter_liked_items" in json_data.keys():
+        filter_liked_items = json_data["filter_liked_items"]
+    else:
+        filter_liked_items = False
+
+    if "number_items" in json_data.keys():
+        number_items = json_data["number_items"]
+    else:
+        number_items = 5
+
+
+    recommendations = model.recommend(user_id, number_items, filter_liked_items)
     recommendations = [int(i) for i in recommendations] 
 
     return jsonify({"ids" : recommendations})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
--- model.py
+++ model.py
@@ -5,6 +5,6 @@ dir_path = "static/model/"
 model = load(dir_path+"model_cpu.joblib")
 user_clicks = load(dir_path+"user_clicks.joblib")
 
-def recommend(user_id):
-    ids, scores = model.recommend(user_id, user_clicks[user_id], N=10, filter_already_liked_items=True)
+def recommend(user_id, number_items=5, filter_already_liked_items=False):
+    ids, scores = model.recommend(user_id, user_clicks[user_id], N=number_items, filter_already_liked_items=filter_already_liked_items)
     return ids
